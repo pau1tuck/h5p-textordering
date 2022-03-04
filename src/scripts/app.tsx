b324c069f1e0bfeb497257cb3a98974f73e530fc
Paul Tuck <pau1tuck@outlook.com>
2022-03-04 21:16:18 +0800

    "2022-03-04_21:16_CST"

--- src/scripts/app.tsx
+++ src/scripts/app.tsx
@@ -9,15 +9,18 @@ export default class TextOrdering extends H5P.EventDispatcher {
    * @param {number} contentId Content's id.
    * @param {object} [extras] Saved state, metadata, etc.
    */
-  constructor(params: any, id: string, extras = {}) {
+  constructor(private params: any, private id: string, private extras = {}) {
     super();
     this.element = document.createElement("div");
     this.element.innerText = params.question;
-    this.element.append("<p>Arnooser</p>");
+    this.element.append("Arnooser");
+
+    // Create render root
+    this.root = document.createElement("div");
 
     this.reactDom = (
       <div>
-        <p>Dogshaver: {this.element}</p>
+        <p>Dogshaver</p>
       </div>
     );
 
@@ -26,9 +29,13 @@ export default class TextOrdering extends H5P.EventDispatcher {
      *
      * @param {jQuery} $wrapper Content's container.
      */
-    this.attach = ($wrapper: JQuery) => {
-      $wrapper?.get(0)?.appendChild(this.element);
-      // ReactDOM.render(this.reactDom, $wrapper.get(0));
+    this.attach = (wrapper: JQuery) => {
+      wrapper.get(0)?.appendChild(this.root);
+
+      // We render an initial state of the content type here. It will be updated
+      // later when the data from the server has arrived.
+      ReactDOM.render(<div>Hello, {this.params.question}.</div>, this.root);
+      this.triggerResize();
     };
   }
 }
