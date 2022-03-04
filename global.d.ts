5a07e3f90764f39e058097a802724ea16d7f2370
Paul Tuck <pau1tuck@outlook.com>
2022-03-04 19:05:31 +0800

    "2022-03-04_19:05_CST"

---
+++ global.d.ts
@@ -0,0 +1,2 @@
+declare let H5P: any;
+declare let H5PIntegration: any;
---
+++ src/entries/main.ts
@@ -0,0 +1,5 @@
+import TextOrdering from "../scripts/app";
+
+// Load library
+H5P = H5P || {};
+H5P.TextOrdering = TextOrdering;
